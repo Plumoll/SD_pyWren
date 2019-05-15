d402cb5d9c7cd3918f7f53432896f1e792431f0a
Magí Tell Bonet <mtellb@gmail.com>
2019-05-15 18:26:07 +0200

    main updated

--- pipeline_map_example.py
+++ pipeline_map_example.py
@@ -6,28 +6,69 @@ guillem frisach
 import pywren_ibm_cloud as pywren
 import yaml
 import pika
+import random
 
-class callback_rabbit:
-    def __init__(self):
+#done but not tested
+class callback_rabbit_leader:
+    def __init__(self, id, max_messages):
+        self.__max_messages = max_messages
         self.__numerao = []
+        self.__num_messages = 0
     def __call__(self, ch, method, properties, body):
+        self.__num_messages += 1
         self.__numerao.append(int(body.decode('UTF-8')))
-        ch.stop_consuming()
+        if self.__num_messages is self.__max_messages:
+            id  = random.randint(0, len(self.__numerao)-1)
+            self.__num_messages = 0
+            self.__max_messages -= 1
+            self.__numerao = []
+            ch.stop_consuming()
+            ch.basic_publish(exchange='sd', routing_key='', body=id)
     def getNumerao(self):
         return self.__numerao
 
+#not finished __call__
+class callback_rabbit_slave:
+    def __init__(self, id, max_messages):
+        self.__max_messages = max_messages
+        self.__numerao = [f'({id})']
+        self.__num_messages = 0
+        self.__id = id
+        self.__active = True
+    def __call__(self, ch, method, properties, body):
+        num = int(body.decode('UTF-8'))
+        #id
+        if self.__num_messages < self.__max_messages:
+            if num < 0:
+                if -num == self.__id:
+                    message = random.randint(0, 100)
+                    ch.basic_publish(exchange='sd', routing_key='', body=message)
+                    self.__active = False
+            #num
+            else:
+                self.__num_messages += 1
+                self.__numerao.append(int(body.decode('UTF-8')))
 
-def my_map_function(id, url):
+                self.__max_messages -= 1
+                ch.stop_consuming()
+    def getNumerao(self):
+        return self.__numerao
+
+#we need to implement both callbacks and a private queue for leader
+def my_map_function(id):
     #return x + 5
-    queue_id = f'queue{id}'
+    queue_id = f'queue{id}3'
+    url = 'amqp://xbjymxoa:jdlKHnEzsJ3woxT8wHGtox-8PI7kJXwW@caterpillar.rmq.cloudamqp.com/xbjymxoa'
     params = pika.URLParameters(url)
     connection = pika.BlockingConnection(params)
     channel = connection.channel()
-    callback = callback_rabbit()
+    channel.exchange_declare(exchange='sd', exchange_type='fanout')
+    callback = callback_rabbit(id)
     #a different queue for each function
     channel.queue_declare(queue_id)
-
-    #each queue has its own callback
+    channel.queue_bind(exchange='sd', queue=queue_id)
+    if id is not 0:
+        channel.basic_publish(exchange='sd', routing_key='', body=id)
     channel.basic_consume(callback, queue=queue_id, no_ack=True)
     #start receiving messages
     channel.start_consuming()
@@ -36,15 +77,15 @@ def my_map_function(id, url):
     connection.close()
     return callback.getNumerao()
 
-def my_map_function(id, url):
-    
-
+#we need to call two different functions, leader and slave
 if __name__ == '__main__':
     #load config file
     with open('ibm_cloud_config', 'r') as config_file:
         res = yaml.safe_load(config_file)
     url = res['rabbitmq']['url']
-    iteration_data = [[1,url], [2,url], [3, url], [4, url]]
+    #iteration_data = [[0,url], [1,url], [2,url], [3, url], [4, url]]
+    #iteration_data = [1, 2, 3, 4, 5, 6, 7, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    iteration_data = [0,'fortnite', 'is', 'better', 'than', 'apex']
     pw = pywren.ibm_cf_executor()
     pw.map(my_map_function, iteration_data)
     print(pw.get_result())

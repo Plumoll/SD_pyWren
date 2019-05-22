6d3eba1ce8fbcf91cdc92436c19078b095ec2d80
Magí Tell Bonet <mtellb@gmail.com>
2019-05-22 11:25:43 +0200

    Update multiple_queue_cs.py

--- multiple_queue_cs.py
+++ multiple_queue_cs.py
@@ -7,35 +7,34 @@ import pika
 import random
 import json
 import os
+import sys
 
 #done but not tested
-class callback_rabbit_leader:
+class callback_rabbit_master:
     def __init__(self, max_messages):
         self.__active_slaves = max_messages
-        self.__numerao = []
+        self.__workers_id = []
         self.__num_messages = 0
     def __call__(self, ch, method, properties, body):
         self.__num_messages += 1
-        self.__numerao.append(int(body.decode('UTF-8')))
+        self.__workers_id.append(int(body.decode('UTF-8')))
         #num = int(body.decode('UTF-8'))
-        #print(f'missatge callback leader: {num}')
+        #print(f'missatge callback master: {num}')
         if self.__num_messages is self.__active_slaves:
-            #print(f'numerao: {self.__numerao}')
-            id = random.randint(0, len(self.__numerao)-1)
-            id = -self.__numerao[id]
+            #print(f'result: {self.__workers_id}')
+            id = random.randint(0, len(self.__workers_id)-1)
+            id = -self.__workers_id[id]
             self.__num_messages = 0
             self.__active_slaves -= 1
-            self.__numerao = []
+            self.__workers_id = []
             ch.basic_publish(exchange='sd', routing_key='', body=str(id))
             if self.__active_slaves is 0:
                 ch.stop_consuming()
-    def getNumerao(self):
-        return self.__numerao
 
 #not finished __call__
 class callback_rabbit_slave:
     def __init__(self, id):
-        self.__numerao = [f'({id})']
+        self.__result = [f'({id})']
         self.__num_messages = -1
         self.__id = id
         self.__active = True
@@ -54,33 +53,33 @@ class callback_rabbit_slave:
                 ch.basic_publish(exchange='sd', routing_key='', body=str(message))
                 self.__active = False
         else:
-            self.__numerao.append(int(body.decode('UTF-8')))
+            self.__result.append(int(body.decode('UTF-8')))
             ch.stop_consuming()
 
-    def get_numerao(self):
-        return self.__numerao
+    def get_result(self):
+        return self.__result
     def get_num_messages(self):
         return self.__num_messages
     def is_active(self):
         return self.__active
 
-def my_function_leader(num_slaves):
+def my_function_master(num_slaves):
     pywren_Conf =  json.loads(os.environ.get('PYWREN_CONFIG', ''))
     url = pywren_Conf['rabbitmq']['amqp_url']
-    callback = callback_rabbit_leader(num_slaves)
+    callback = callback_rabbit_master(num_slaves)
     params = pika.URLParameters(url)
     connection = pika.BlockingConnection(params)
     channel = connection.channel()
-    channel.queue_declare('queue_leaderxxx3')
+    channel.queue_declare('queue_masterxxx3')
     channel.exchange_declare(exchange='sd', exchange_type='fanout')
     for id in range(0, num_slaves):
         queue_id = f'queue{id}xxx3'
         channel.queue_declare(queue_id)
         channel.queue_bind(exchange='sd', queue=queue_id)
     channel.basic_publish(exchange='sd', routing_key='', body=str(num_slaves))
-    channel.basic_consume(callback, queue='queue_leaderxxx3', no_ack=True)
+    channel.basic_consume(callback, queue='queue_masterxxx3', no_ack=True)
     channel.start_consuming()
-    return callback.getNumerao()
+    return []
 
 def my_function_slave(id):
     queue_id = f'queue{id}xxx3'
@@ -99,18 +98,21 @@ def my_function_slave(id):
     for _ in range(0, callback.get_num_messages()):
         #print(f'numero iteracio: {_}')
         if(callback.is_active()):
-            channel.basic_publish(exchange='', routing_key='queue_leaderxxx3', body=str(id))
+            channel.basic_publish(exchange='', routing_key='queue_masterxxx3', body=str(id))
         channel.basic_consume(callback, queue=queue_id, no_ack=True)
         channel.start_consuming()
 
-    return callback.get_numerao()
+    return callback.get_result()
 
 
-#we need to call two different functions, leader and slave
+#we need to call two different functions, master and slave
 if __name__ == '__main__':
     #load config file
+    num_workers = 10
+    if len(sys.argv) == 2:
+        num_workers = int(sys.argv[1])
     pw = pywren.ibm_cf_executor(rabbitmq_monitor=True)
-    pw.map(my_function_slave, range(3))
+    pw.map(my_function_slave, range(num_workers))
     pw1 = pywren.ibm_cf_executor(rabbitmq_monitor=True)
-    pw1.call_async(my_function_leader, 3)
+    pw1.call_async(my_function_master, num_workers)
     print(pw.get_result())
